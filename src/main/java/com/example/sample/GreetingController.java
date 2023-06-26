121baa2af154d772ec1643476eeaad4ab0be03fb
Raghupatik <ragupathi.kommidi@gmail.com>
2023-06-26 10:29:47 -0500

    yes

--- src/main/java/com/example/sample/GreetingController.java
+++ src/main/java/com/example/sample/GreetingController.java
@@ -3,6 +3,6 @@ package com.example.sample;
 
 public class GreetingController {
     public String greeting() {
-        return "Lambda is Executed Successfully with Version 1.0.1 !!";
+        return "Lambda is Executed Successfully with Version 1.0.2 !!";
     }
 }
